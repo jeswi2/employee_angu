ee91f47af71e48f76d89a0123c1b3acc93670334
jeswi2 <jeswimathewm@gmail.com>
2022-10-21 09:41:27 +0530

    se

---
+++ src/app/api.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http:HttpClient) { }
+
+  viewemployee=()=>{
+    return this.http.get("http://localhost:8080/viewemployee")
+  }
+  
+    addemployee=(data:any)=>{
+      return this.http.post("http://localhost:8080/addemployee",data)
+    }
+  
+    deleteemployee=(data:any)=>{
+  return this.http.post("http://localhost:8080/deleteemployee",data)
+    }
+  
+}
